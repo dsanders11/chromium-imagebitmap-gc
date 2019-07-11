8df5a93c6c28c9175987951a03150ae940681f03
David Sanders <dsanders11@ucsbalum.com>
2019-07-11 14:11:36 -0700

    Trying to find a suitable reproduce case

--- worker.js
+++ worker.js
@@ -1,7 +1,15 @@
+const imageBitmaps = [];
+
 function ack () {
   postMessage('ACK')
 }
 
 onmessage = async (event) => {
+  imageBitmaps.push(event.data.imageBitmap);
+
+  if (imageBitmaps.length > 10) {
+    imageBitmaps.shift();
+  }
+
   setTimeout(ack, 40);
 }
