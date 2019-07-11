71fea38a3bc6b5f8e4b29ab6e41eb8d44c577b49
David Sanders <dsanders11@ucsbalum.com>
2019-07-11 13:23:39 -0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,4 +1,8 @@
-onmessage = async () => {
+onmessage = async (event) => {
+  // Dispose of the data, this should avoid
+  // a major GC
+  event.data.imageBitmap.close();
+
   setTimeout(() => {
     postMessage('ACK')
   }, 40);
