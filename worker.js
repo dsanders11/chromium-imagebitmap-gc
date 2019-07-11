de5243b280a262c319389dbc1927c9b111a0731d
David Sanders <dsanders11@ucsbalum.com>
2019-07-11 13:45:54 -0700

    Minimize garbage created

--- worker.js
+++ worker.js
@@ -1,9 +1,11 @@
+function ack () {
+  postMessage('ACK')
+}
+
 onmessage = async (event) => {
   // Dispose of the data, this should avoid
   // a major GC
   event.data.imageBitmap.close();
 
-  setTimeout(() => {
-    postMessage('ACK')
-  }, 40);
+  setTimeout(ack, 40);
 }
