3b65c88199f9bf88c8120a315ca841a58d81f639
David Sanders <dsanders11@ucsbalum.com>
2019-07-11 14:46:39 -0700

    Add start stop functionality

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const worker = new Worker('./worker.js');
 
 const imageData = new ImageData(1920, 1080);
 
+let running = false;
+
 async function transferImageBitmap () {
   const imageBitmap = await createImageBitmap(imageData);
 
@@ -11,10 +13,20 @@ async function transferImageBitmap () {
 }
 
 worker.onmessage = () => {
-  transferImageBitmap();
+  if (running) {
+    transferImageBitmap();
+  }
 };
 
-document.getElementById('start').onclick = () => {
-  // Start the infinite run
-  transferImageBitmap();
+document.getElementById('action').onclick = () => {
+  running = !running;
+
+  if (running) {
+    document.getElementById('action').innerText = 'Stop';
+
+    // Start the infinite run
+    transferImageBitmap();
+  } else {
+    document.getElementById('action').innerText = 'Start';
+  }
 }
