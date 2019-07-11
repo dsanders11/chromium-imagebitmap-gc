34d8df905a3af50a0b429331c662f7ed90e82aa5
David Sanders <dsanders11@ucsbalum.com>
2019-07-11 16:12:50 -0700

    More experimenting

--- main.js
+++ main.js
@@ -2,11 +2,18 @@ const worker = new Worker('./worker.js');
 
 const imageData = new ImageData(1920, 1080);
 
+const imageBitmaps = [];
+
 let running = false;
 let transfer = true;
 
 async function sendImageBitmap () {
   const imageBitmap = await createImageBitmap(imageData);
+  imageBitmaps.push(imageBitmap);
+
+  if (imageBitmaps.length > 10) {
+    imageBitmaps.shift();
+  }
 
   let transferables = undefined;
 
@@ -15,8 +22,8 @@ async function sendImageBitmap () {
   }
 
   worker.postMessage({
-    imageBitmap
-  }, transferables);
+    message: 'PING'
+  });
 }
 
 worker.onmessage = () => {
