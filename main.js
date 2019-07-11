8ced8acb2e22c9f73fc3fac5dc6fe0dd4a114b76
David Sanders <dsanders11@ucsbalum.com>
2019-07-11 15:19:28 -0700

    Add toggle for transfering or copying

--- main.js
+++ main.js
@@ -3,20 +3,31 @@ const worker = new Worker('./worker.js');
 const imageData = new ImageData(1920, 1080);
 
 let running = false;
+let transfer = true;
 
-async function transferImageBitmap () {
+async function sendImageBitmap () {
   const imageBitmap = await createImageBitmap(imageData);
+
+  let transferables = undefined;
+
+  if (transfer) {
+    transferables = [ imageBitmap ];
+  }
 
   worker.postMessage({
     imageBitmap
-  }, [ imageBitmap ]);
+  }, transferables);
 }
 
 worker.onmessage = () => {
   if (running) {
-    transferImageBitmap();
+    sendImageBitmap();
   }
 };
+
+document.getElementById('transfer').onchange = (event) => {
+  transfer = event.target.checked;
+}
 
 document.getElementById('action').onclick = () => {
   running = !running;
@@ -25,7 +36,7 @@ document.getElementById('action').onclick = () => {
     document.getElementById('action').innerText = 'Stop';
 
     // Start the infinite run
-    transferImageBitmap();
+    sendImageBitmap();
   } else {
     document.getElementById('action').innerText = 'Start';
   }
